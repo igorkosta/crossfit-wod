cf142c67c52601f4400e88761ca0c92b4dfed23a
Igor Kostyuchenok <kostyuchenok@fidor.de>
2016-10-23 01:31:55 +0200

    - added tests - added links to `prev` and `next` wod’s
    not a complete rewrite but close to it

--- test/index.js
+++ test/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const Wod = require('../lib/index');
+const Sinon = require('sinon');
+const Request = require('request');
 const Code = require('code');   // assertion library
 const Lab = require('lab');
+const Wod = require('../lib/index');
+const Fixtures = require('./fixtures');
 const lab = exports.lab = Lab.script();
 
 const describe = lab.describe;
+const afterEach = lab.afterEach;
 const it = lab.it;
 const expect = Code.expect;
 
@@ -28,14 +32,70 @@ describe('randomDate()', () => {
         expect(Wod.randomDate()).to.be.a.within(new Date('2001-02-10'), new Date());
         done();
     });
+});
+
+describe('randomWod()', () => {
+
+    it('should return a console.log with a random wod', (done) => {
+
+        // should cover line 61
+        done();
+    });
 
 });
 
 describe('Unsuccessful request', () => {
 
+    afterEach( (done) => {
+
+        Request.get.restore();
+        done();
+    });
+
     it('should search for a successful url and log it to the console', (done) => {
+
+        const error = new Error('connect ETIMEDOUT');
+        Sinon.stub(Request, 'get').yields(error, { statusCode: 500 }, []);
+
+        Wod.randomWod( (result) => {
+
+            expect(result).to.equal(error);
+        });
 
         done();
     });
+
+    it('should search for a successful url and log it to the console', (done) => {
+
+        const html = Fixtures.wod;
+        const expected = { title: 'Sunday 160508',
+                           href: 'https://crossfit.com/workout/2016/05/08',
+                           excercises: 'Deadlift 5-5-3-3-3-1-1-1-1 repsPost loads to comments.',
+                           picture_url: undefined,
+                           next: 'https://crossfit.com/workout/2016/05/09',
+                           previous: 'https://crossfit.com/workout/2016/05/07' };
+        Sinon
+            .stub(Request, 'get')
+            .yields(null, { statusCode: 200 }, html);
+        Wod.randomWod( (result) => {
+
+            expect(result).to.equal(expected);
+        });
+        done();
+    });
+
+    // it('hits a 404', (done) => {
+    //     // should cover the case where first request hits a 404
+    //     const notFound = Fixtures.notFound;
+    //     Sinon
+    //         .stub(Request, 'get')
+    //         .yields(null, { statusCode: 404 }, notFound);
+    //
+    //     Wod.randomWod( (result) => {
+    //
+    //         expect(result).to.equal(expected);
+    //     });
+    //     done();
+    // });
 
 });
